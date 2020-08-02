85b92f1a9325360619c871b9bee6ce84d48532bb
“Shubham” <shubhampattar2@gmail.com>
2020-08-02 20:53:00 +0530

    ADDED Code To Validate Last Name.

--- UserRegistration.js
+++ UserRegistration.js
@@ -3,8 +3,30 @@ const prompt = require('prompt-sync')();
 // RegEx Formats
 let nameFormat = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
 
-const name = prompt("Enter Firs Name: ");
-if(nameFormat.test(name))
-    console.log(`Hello ${name}`);
-else
-    console.log("Enter Valid Name");
+// Variables
+let firstName = "";
+let lastName = "";
+
+function checkFormat(input, inputFormat){
+    return inputFormat.test(input);
+}
+
+function getFirstName(){
+    firstName = prompt("Enter First Name: ");
+    if(!checkFormat(firstName, nameFormat)){
+        console.log("Enter Valid First Name");
+        getFirstName();
+    }
+    return;
+}
+
+function getLastName(){
+    lastName = prompt("Enter Last Name: ");
+    if(!checkFormat(lastName, nameFormat)){
+        console.log("Enter Valid Last Name");
+        getLastName();
+    }
+    return
+}
+getFirstName();
+getLastName();
